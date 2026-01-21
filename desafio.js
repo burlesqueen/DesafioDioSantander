0012acd751e9eef8eba4980ef1beffadd5518fb3
Livia Vila Bela <liviavilabela@gmail.com>
2026-01-21 20:02:45 -0300

    feat: adiciona calculadora de partidas rankeadas

---
+++ desafio.js
@@ -0,0 +1,29 @@
+function calculateRankedLevel(wins, losses) {
+    let balance = wins - losses;
+    let level = "";
+
+    if (wins < 10) {
+        level = "Ferro";
+    } else if (wins >= 11 && wins <= 20) {
+        level = "Bronze";
+    } else if (wins >= 21 && wins <= 50) {
+        level = "Prata";
+    } else if (wins >= 51 && wins <= 80) {
+        level = "Ouro";
+    } else if (wins >= 81 && wins <= 90) {
+        level = "Esmeralda";
+    } else if (wins >= 91 && wins <= 100) {
+        level = "Diamante";
+    } else if (wins >= 101) {
+        level = "Mestre Desafiante";
+    }
+
+    return "O Heroi tem de saldo de " + balance + " esta no nivel de " + level;
+}
+
+
+let wins = 75;
+let losses = 20;
+
+let result = calculateRankedLevel(wins, losses);
+console.log(result);
